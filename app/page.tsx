689716e56a7a745a3fae7152b533a8ac0de5156f
kazuki-310 <kazu.0310123@gmail.com>
2024-12-08 17:14:15 +0900

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import AcmeLogo from '@/app/ui/acme-logo'
-import heroDesktop from '@/public/hero-desktop.png'
 import { ArrowRightIcon } from '@heroicons/react/24/outline'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -27,12 +26,10 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
           <Image
-            // src="/hero-desktop.png"
-            src={heroDesktop}
-            // width={1000}
-            // height={760}
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop version"
           />
