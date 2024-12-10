e388e8265e2774e4355fecd82c62156deda5aba1
kazuki-310 <kazu.0310123@gmail.com>
2024-12-10 23:19:04 +0900

    インボイスページに検索機能を追加し、顧客テーブルのインポート順序を整理

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -4,7 +4,10 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
 import { useQueryState } from 'nuqs'
 
 export default function Search({ placeholder }: { placeholder: string }) {
-  const [query, setQuery] = useQueryState('query')
+  const [query, setQuery] = useQueryState('query', {
+    shallow: false,
+    throttleMs: 500,
+  })
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
