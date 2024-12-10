1c6145e55587442e0d3c7e6855fc6469e9b2ae05
kazuki-310 <kazu.0310123@gmail.com>
2024-12-10 22:49:55 +0900

    add nuqs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { inter } from '@/app/ui/fonts'
 import '@/app/ui/global.css'
+import { NuqsAdapter } from 'nuqs/adapters/next/app'
 
 export default function RootLayout({
   children,
@@ -8,7 +9,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        <NuqsAdapter>{children}</NuqsAdapter>
+      </body>
     </html>
   )
 }
